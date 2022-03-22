b11ca407f937c5adced3a200a2ccf8e72aa79b42
mybabygrand <amuro_ray@kakao.com>
2022-03-22 22:44:26 +0900

    init

---
+++ src/main/java/me/mybabygrand/class_java8/ClassJava8Application.java
@@ -0,0 +1,13 @@
+package me.mybabygrand.class_java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassJava8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassJava8Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/mybabygrand/class_java8/ClassJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mybabygrand.class_java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassJava8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
